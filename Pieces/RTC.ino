d6c80200f04a081f7ebc459421258f964ba077f7
Frederik B. E. Larsen <frederik@bjoerholm.dk>
2016-06-20 14:57:29 +0200

    rename and RTC

---
+++ Pieces/RTC.ino
@@ -0,0 +1,28 @@
+#include <DS3231.h>
+DS3231  rtc(SDA, SCL);
+
+void setup() {
+  Serial.begin(115200);
+  rtc.begin();
+  // The following lines can be uncommented to set the date and time
+  //rtc.setDOW(WEDNESDAY);     // Set Day-of-Week to SUNDAY
+  //rtc.setTime(12, 0, 0);     // Set the time to 12:00:00 (24hr format)
+  //rtc.setDate(1, 1, 2014);   // Set the date to January 1st, 2014
+
+}
+
+void loop() {
+  // Send Day-of-Week
+  Serial.print(rtc.getDOWStr());
+  Serial.print(" ");
+  
+  // Send date
+  Serial.print(rtc.getDateStr());
+  Serial.print(" -- ");
+
+  // Send time
+  Serial.println(rtc.getTimeStr());
+  
+  // Wait one second before repeating :)
+  delay (1000);
+}
